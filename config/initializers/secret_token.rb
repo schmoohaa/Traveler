0540ee9452c7c1893dac1e58f6127ec3f89e4a2a
Tom Werth <thomas_werth@hotmail.com>
2011-03-02 13:19:52 -0600

    Initial Rails3 commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Traveler::Application.config.secret_token = '51af3f83ac2847be23e49878067dfe038c403c145e311515680da7aca19a14ee248fb56d51a090284531662d8a38b3b07b6c8310b5d7661c8b363f0f07b5abf1'
